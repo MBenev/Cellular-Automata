126fae4a4fc7e7d017e204cd4fd333a9951592a2
Mihail Benev <60147762+MBenev@users.noreply.github.com>
2021-10-27 19:54:53 +0300

    Connecting regions started
    Started work on connecting regions

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -82,6 +82,7 @@ public class MapGenerator : MonoBehaviour
 
         List<List<Coord>> roomRegions = GetRegions(0);
         int roomThresholdSize = 50; // if region is under 50 empty tiles remove it
+        List<Room> survivingRooms = new List<Room>();
         foreach (List<Coord> roomRegion in roomRegions)
         {
             if (roomRegion.Count < roomThresholdSize)
@@ -91,7 +92,16 @@ public class MapGenerator : MonoBehaviour
                     map[tile.tileX, tile.tileY] = 1;
                 }
             }
+            else
+            {
+                survivingRooms.Add(new Room(roomRegion, map));
+            }
         }
+    }
+
+    void ConnectClosestRooms(List<Room> allRooms)
+    {
+
     }
 
     List<List<Coord>> GetRegions(int tileType)
@@ -236,19 +246,48 @@ public class MapGenerator : MonoBehaviour
 
     }
 
-    void OnDrawGizmos()
+    class Room
     {
-        /*
-		if (map != null) {
-			for (int x = 0; x < width; x ++) {
-				for (int y = 0; y < height; y ++) {
-					Gizmos.color = (map[x,y] == 1)?Color.black:Color.white;
-					Vector3 pos = new Vector3(-width/2 + x + .5f,0, -height/2 + y+.5f);
-					Gizmos.DrawCube(pos,Vector3.one);
-				}
-			}
-		}
-		*/
+        public List<Coord> tiles;
+        public List<Coord> edgeTiles;
+        public List<Room> connectedRooms;
+        public int roomSize;
+
+        public Room() { }
+
+        public Room(List<Coord> roomTiles, int[,] map)
+        {
+            tiles = roomTiles;
+            roomSize = tiles.Count;
+            connectedRooms = new List<Room>();
+
+            edgeTiles = new List<Coord>();
+            foreach (Coord tile in tiles)
+            {
+                for (int x = tile.tileX - 1; x <= tile.tileX +1; x++)
+                {
+                    for (int y = tile.tileY - 1; y <= tile.tileY + 1; y++)
+                    {
+                        if (x == tile.tileX || y == tile.tileY) // excluding diagonal neighbours
+                        {
+                            if (map[x,y] == 1) // if current is wall
+                            {
+                                edgeTiles.Add(tile);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public static void ConnectRooms(Room roomA, Room roomB)
+        {
+            roomA.connectedRooms.Add(roomB);
+            roomB.connectedRooms.Add(roomA);
+        }
+        public bool IsConnected(Room otherRoom)
+        {
+            return connectedRooms.Contains(otherRoom);
+        }
     }
 
 }
