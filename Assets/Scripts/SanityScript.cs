e87d6dc65b949c8ee1da6dfeb29b1d6fedbf11ff
Mihail Benev <60147762+MBenev@users.noreply.github.com>
2021-11-23 19:08:30 +0200

    Event happens
    Event happens every 5 seconds if the sanity is still lower than 50%.
    Fixed bug where it didn't stop happening.
    Fixed bug where it happened only twice.

--- Assets/Scripts/SanityScript.cs
+++ Assets/Scripts/SanityScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,11 +10,15 @@ public class SanityScript : MonoBehaviour
     float maxSanity = 100f;
     public static float sanity;
     public GameObject eventButton;
+    WaitTime waitTimeScript;
+    public GameObject player;
+    bool happenedInTheLast5Seconds = false;
 
     private void Start()
     {
         sanityMeter = GetComponent<Image>();
         sanity = maxSanity;
+        waitTimeScript = player.GetComponent<WaitTime>();
     }
 
     private void Update()
@@ -34,12 +39,35 @@ public class SanityScript : MonoBehaviour
             if (sanity < 50f)
             {
                 sanityMeter.GetComponent<Image>().color = new Color(139, 0, 0);
-                eventButton.SetActive(true);
+                ShowEvent();
             }
         }
         if(sanity > 50f)
         {
             sanityMeter.GetComponent<Image>().color = new Color(161, 161, 161);
         }
+        
+    }
+
+    public void EnableTimeScript()
+    {
+        waitTimeScript.enabled = true;
+    }
+
+    public void AfterTimePasses()
+    {
+        happenedInTheLast5Seconds = false;
+        waitTimeScript.enabled = false;
+    }
+
+    private void ShowEvent()
+    {
+        if(!happenedInTheLast5Seconds)
+        {
+            Debug.Log("After 5 second is false");
+            eventButton.SetActive(true);
+            happenedInTheLast5Seconds = true;            
+        }
+
     }
 }
---
+++ Assets/Scripts/WaitTime.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitTime : MonoBehaviour
+{
+    public GameObject sanityMeter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    private void OnEnable()
+    {
+        StartCoroutine(Example());
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator Example()
+    {
+        yield return new WaitForSecondsRealtime(5);
+        Debug.Log("time passed");
+        sanityMeter.GetComponent<SanityScript>().AfterTimePasses();
+    }
+}
